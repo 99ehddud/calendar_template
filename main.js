b911c88c85f5b43324414d35a012c057b4508355
99ehddud <99ehddud@gmail.com>
2022-02-22 09:01:25 +0900

    Template Ver.x.1.4 (Fix Table)

--- main.js
+++ main.js
@@ -24,83 +24,83 @@ function weeks_of_month(yyyy, MM) {
 }
 
 function make_calendar(yyyy, MM) {
-    document.write("<table>");
-
-    let year_month = yyyy + ". " + MM;
+    let year_month = yyyy + '. ' + MM;
     document.getElementById('year_month').textContent = year_month;
 
-    let day = ["SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"]
-    document.write("<tr>");
-    for (var i = 0; i <= 6; i++) {
-        document.write("<td class=\"day_of_the_week\">" + day[i] + "</td>");
-    }
-    document.write("</tr>");
+    document.write('<table id=\'calendar\'>');
 
-    document.write("<tr>");
+    const day_of_the_week = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+    document.write('<tr>');
+    for (var h = 0; h <= 6; h++) {
+        document.write('<td class=\'day_of_the_week\'>' + day_of_the_week[h] + '</td>');
+    }
+    document.write('</tr>');
+
     if (day_of_week(yyyy, MM, 1) != 0) {
-        for (var j = 1; j <= day_of_week(yyyy, MM, 1); j++) {
-            document.write("<td></td>")
+        document.write('<tr>');
+        for (var i = 1; i <= day_of_week(yyyy, MM, 1); i++) {
+            document.write('<td></td>')
         }
     }
 
     for (var dd = 1; dd <= finalday_of_Month(yyyy, MM); dd++) {
         if (day_of_week(yyyy, MM, dd) == 0) {
-            document.write("<tr>");
+            document.write('<tr>');
         }
-        document.write("<td class=\"")
+        document.write('<td class=\'')
 
         if (day_of_week(yyyy, MM, dd) == 0) {
             if (weeks_of_month(yyyy, MM) == 4) {
-                document.write("sunday_4week");
+                document.write('sunday_4week');
             } else if (weeks_of_month(yyyy, MM) == 5) {
-                document.write("sunday_5week");
+                document.write('sunday_5week');
             } else if (weeks_of_month(yyyy, MM) == 6) {
-                document.write("sunday_6week");
+                document.write('sunday_6week');
             }
         } else if (day_of_week(yyyy, MM, dd) == 6) {
             if (weeks_of_month(yyyy, MM) == 4) {
-                document.write("saturday_4week");
+                document.write('saturday_4week');
             } else if (weeks_of_month(yyyy, MM) == 5) {
-                document.write("saturday_5week");
+                document.write('saturday_5week');
             } else if (weeks_of_month(yyyy, MM) == 6) {
-                document.write("saturday_6week");
+                document.write('saturday_6week');
             }
         } else {
             if (weeks_of_month(yyyy, MM) == 4) {
-                document.write("weekday_4week");
+                document.write('weekday_4week');
             } else if (weeks_of_month(yyyy, MM) == 5) {
-                document.write("weekday_5week");
+                document.write('weekday_5week');
             } else if (weeks_of_month(yyyy, MM) == 6) {
-                document.write("weekday_6week");
+                document.write('weekday_6week');
             }
         }
 
-        document.write("\">");
+        document.write('\'>');
         if (yyyy == today_year && MM == today_month && dd == today_date) {
             if (day_of_week(yyyy, MM, dd) == 0) {
-                document.write("<span style=\"color: red; font-weight: 900;\">" + dd + "</span>");
+                document.write('<span style=\'color: red; font-weight: 900;\'>' + dd + '</span>');
             } else if (day_of_week(yyyy, MM, dd) == 6) {
-                document.write("<span style=\"color: blue; font-weight: 900;\">" + dd + "</span>");
+                document.write('<span style=\'color: blue; font-weight: 900;\'>' + dd + '</span>');
             } else {
-                document.write("<span style=\"color: black; font-weight: 900;\">" + dd + "</span>");
+                document.write('<span style=\'color: black; font-weight: 900;\'>' + dd + '</span>');
             }
 
         } else {
             document.write(dd);
         }
-        document.write("</td>");
+        document.write('</td>');
 
         if (day_of_week(yyyy, MM, dd) != 6 && dd == finalday_of_Month(yyyy, MM, dd)) {
-            for (var k = day_of_week(yyyy, MM, dd); k < 6; k++){
-                document.write("<td></td>");
+            for (var j = day_of_week(yyyy, MM, dd); j < 6; j++){
+                document.write('<td></td>');
             }
         }
 
         if (day_of_week(yyyy, MM, dd) == 6) {
-            document.write("</tr>");
+            document.write('</tr>');
         }
     }
-    document.write("</table>");
+    document.write('</table>');
 }
 
 // For Resize
@@ -112,38 +112,36 @@ function resize(yyyy, MM) {
     const aside = document.querySelector('aside');
     const main = document.querySelector('main');
     const calendartitle = document.querySelector('#calendar_title');
-    const year_month = document.querySelector('#year_month');
+    const yearmonth = document.querySelector('#year_month');
+    const dayoftheweek = document.querySelector('.day_of_the_week');
+    const calendar = document.querySelector('#calendar');
     const footer = document.querySelector('footer');
 
     let headerHeight = header.offsetHeight;
     let asideWidth = aside.offsetWidth;
     let calendartitleHeight = calendartitle.offsetHeight;
+    let dayoftheweekHeight = dayoftheweek.offsetHeight;
     let footerHeight = footer.offsetHeight;
-    let calendarHeight = innerHeight - headerHeight - calendartitleHeight - weeks_of_month(yyyy, MM) - footerHeight;
+    let calendarHeight = innerHeight - headerHeight - calendartitleHeight - dayoftheweekHeight - weeks_of_month(yyyy, MM) - footerHeight;
 
     header.style.width = innerWidth + 'px';
     main.style.width = (innerWidth - asideWidth) + 'px';
     main.style.height = (innerHeight - headerHeight - footerHeight) + 'px';
     aside.style.height = (innerHeight - headerHeight - footerHeight) + 'px';
-    year_month.style.width = (innerWidth - asideWidth - 146) + 'px';
+    yearmonth.style.width = (innerWidth - asideWidth - 146) + 'px';
 
-    for (var i = 0; i <= weeks_of_month(yyyy, MM); i++) {
-        let tr = document.getElementsByTagName('tr')[i];
-        for (var j = 0; j <= 6; j++) {
-            let td = tr.getElementsByTagName('td')[j];
+    for (var k = 0; k <= weeks_of_month(yyyy, MM); k++) {
+        let tr = calendar.getElementsByTagName('tr')[k];
+        for (var l = 0; l <= 6; l++) {
+            let td = tr.getElementsByTagName('td')[l];
             td.style.width = (innerWidth - asideWidth - 6) / 7 + 'px';
-            if (i == 0) {
-                td.style.height = calendarHeight * (10/100) + 'px';
-            } else {
-                td.style.height = (calendarHeight * (90/100)) / weeks_of_month(yyyy, MM) + 'px';
+            if (k != 0) {
+                td.style.height = calendarHeight / weeks_of_month(yyyy, MM) + 'px';
+                td.style.borderTop = '1px solid rgb(180, 180, 180)';
             }
 
-            if (i < weeks_of_month(yyyy, MM)) {
-                td.style.borderBottom = "1px solid rgb(180, 180, 180)";
-            }
-
-            if (j < 6) {
-                td.style.borderRight = "1px solid rgb(180, 180, 180)";
+            if (l < 6) {
+                td.style.borderRight = '1px solid rgb(180, 180, 180)';
             }
         }
     }
